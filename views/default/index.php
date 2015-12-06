06c1f6b0661de362fb3b0ff1ca8a49ebe1f8c4f0
Shelepov G <gvshelepov@gmail.com>
2015-12-06 13:27:49 +0300

    исправления верстки

--- views/default/index.php
+++ views/default/index.php
@@ -187,7 +187,7 @@
                             <div class="form-group"><textarea name="comment" placeholder="Ваше сообщение" class="form-control comment_input"></textarea></div>
                         </div>
                         <button type="submit" class="btn center-block">Заказать</button>
-                        <p class="text-uppercase text-center fs_20 js_any_error"></p>
+                        <p class="text-uppercase text-center fs_20 js_any_error hidden"></p>
                     </form>
                 </div>
             </div>
