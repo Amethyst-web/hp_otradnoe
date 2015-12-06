568f9ad7cc49e03937e9a0189828c861fcb27a2c
HEKET313 <neket313@gmail.com>
2015-12-06 17:21:58 +0300

    Убрал лишние картинки из галереи

--- views/default/index.php
+++ views/default/index.php
@@ -102,9 +102,6 @@
             <p class="h1" ><span class="color_black">Наша</span> <span class="color_red">галерея</span></p>
             <hr class="line">
             <div class="gallery center-block scrollbar-inner">
-                <a href="/img/gallery/big/one_img_big.jpg" data-lightbox="roadtrip">
-                    <img src="/img/gallery/small/one_img_sm.jpg" alt="Image">
-                </a>
                 <a href="/img/gallery/big/1.jpg" data-lightbox="roadtrip">
                     <img src="/img/gallery/small/1.jpg" alt="Image">
                 </a>
@@ -119,24 +116,6 @@
                 </a>
                 <a href="/img/gallery/big/5.jpg" data-lightbox="roadtrip">
                     <img src="/img/gallery/small/5.jpg" alt="Image">
-                </a>
-                                <a href="/img/gallery/big/3.jpg" data-lightbox="roadtrip">
-                    <img src="/img/gallery/small/3.jpg" alt="Image">
-                </a>
-                <a href="/img/gallery/big/4.jpg" data-lightbox="roadtrip">
-                    <img src="/img/gallery/small/4.jpg" alt="Image">
-                </a>
-                <a href="/img/gallery/big/5.jpg" data-lightbox="roadtrip">
-                    <img src="/img/gallery/small/5.jpg" alt="Image">
-                </a>
-                <a href="/img/gallery/big/one_img_big.jpg" data-lightbox="roadtrip">
-                    <img src="/img/gallery/small/one_img_sm.jpg" alt="Image">
-                </a>
-                <a href="/img/gallery/big/1.jpg" data-lightbox="roadtrip">
-                    <img src="/img/gallery/small/1.jpg" alt="Image">
-                </a>
-                <a href="/img/gallery/big/2.jpg" data-lightbox="roadtrip">
-                    <img src="/img/gallery/small/2.jpg" alt="Image">
                 </a>
             </div>
         </div>
